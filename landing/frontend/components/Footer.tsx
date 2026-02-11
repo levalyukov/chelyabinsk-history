d9915c5a7ea8d7b4c1277b9b652a50e875307659
levalyukov <lev.alyukov@gmail.com>
2026-02-11 08:47:16 +0500

    hotfix

--- landing/frontend/components/Footer.tsx
+++ landing/frontend/components/Footer.tsx
@@ -1,15 +1,11 @@
 import "../styles/Footer.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBluesky, faGithub, faTelegram, faTiktok, faVk } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   const media = {
-    0: {id: "vk", icon: faVk, href: ""},
-    1: {id: "telegram", icon: faTelegram, href: ""},
-    2: {id: "github", icon: faGithub, href: ""},
-    3: {id: "bluesky", icon: faBluesky, href: ""},
-    4: {id: "tiktok", icon: faTiktok, href: ""}
+    0: {id: "github", icon: faGithub, href: "https://github.com/levalyukov/chelyabinsk-history"},
   };
   
   const footer = {
--- landing/frontend/components/Start.tsx
+++ landing/frontend/components/Start.tsx
@@ -1,91 +1,34 @@
 import "../styles/Start.css"
 
 export default function Start() {
+  const tewst = {
+    0: {name: "Площадь Революции"},
+    1: {name: "Парк Гагарина"},
+    2: {name: "Кировка"},
+    3: {name: "Краеведческий музей"},
+    4: {name: "Немецкий квартал"},
+    5: {name: "Дворец культуры ЧМК"},
+    6: {name: "Точка интереса"},
+    7: {name: "Точка интереса"},
+    8: {name: "Точка интереса"},
+    9: {name: "Точка интереса"}
+  };
+  
   return (
     <section id="start">
       <h1 id="anim-element">Погрузитесь в историю</h1>
       <p id="anim-element">Все экскурсии и репортажи в одном месте:</p>
       <div className="places-container">
         <div className="places">
-          <article>
+        {Object.entries(tewst).map(([key,index]) => (
+          <article key={key}>
             <img src="" alt="" id="cover"/>
             <div className="info">
-              <h2>Точка интереса</h2>
+              <h2>{index.name}</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
             </div>
           </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
-
-          <article>
-            <img src="" alt="" id="cover"/>
-            <div className="info">
-              <h2>Точка интереса</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
-          </article>
+        ))}
         </div>
       </div>
       <a href="app/">Начать приключение</a>
