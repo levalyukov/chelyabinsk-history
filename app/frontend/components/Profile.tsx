5763b03d90556b15a29e1d7e1675649e10561e23
levalyukov <lev.alyukov@gmail.com>
2026-03-06 07:55:53 +0500

    fix styles

--- app/frontend/components/Profile.tsx
+++ app/frontend/components/Profile.tsx
@@ -6,6 +6,11 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 export default function Profile({setSettings}: 
   {setSettings: (state:boolean) => void}) {
   return (
-    <section className="profile"></section>
+    <section id="user-profile">
+      <div className="profile">
+        <img src="https://i.pinimg.com/736x/b5/46/ce/b546ce5a216eaecbfc4ccfe563750c36.jpg" alt="pfp.jpg" />
+        <h1>Username</h1>
+      </div>
+    </section>
   );  
 };
