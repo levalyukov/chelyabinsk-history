630071621b869ba570b8f32032826fd188839bcd
levalyukov <lev.alyukov@gmail.com>
2026-03-06 21:22:31 +0500

    remove leaflet components

--- app/frontend/components/Map.tsx
+++ app/frontend/components/Map.tsx
@@ -7,34 +7,6 @@ import "../styles/Map.css"
 import MarkerIcon from "../images/marker.svg"
 
 export default function Map({setMap}: {setMap:(map:null) => void}) {
-  // const mapBounds:number[][]= [[55.316877, 61.184491],[55.028634,61.661413]];
-  // const retina = (window.devicePixelRatio === 1) ? false : true;
-  // const icon = new L.Icon({
-  //   iconUrl: MarkerIcon,
-  //   iconSize: [48, 48],
-  //   iconAnchor: [16, 48],
-  //   popupAnchor: [8, -48],
-  // });
-
-  // function MapComponent({setMap}:{setMap:(map:Leaflet | null) => void}) {
-  //   const map = useMap();
-  //   useEffect(() => {
-  //     setMap(map);
-  //   }, [map, setMap]);
-  //   return null;
-  // };
-
-  // function MapBoundsComponent() {
-  //   const map = useMap();
-  //   const [bounds, setBounds] = useState(map.getBounds());
-
-  //   useMapEvents({
-  //     moveend: () => {
-  //       const currentBounds = map.getBounds();
-  //       setBounds(currentBounds);
-  //     },
-  //   }); return null;
-  // };
 
   useEffect(() => {
     if (!mapContainer.current) return;
