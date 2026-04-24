7a8625758de49cc27618c63ffeacff9136eb4816
levalyukov <lev.alyukov@gmail.com>
2026-04-24 20:02:56 +0500

    fix hero section of the landing

--- landing/frontend/components/Hero.tsx
+++ landing/frontend/components/Hero.tsx
@@ -1,24 +1,28 @@
 import "../styles/Hero.css";
 
-import testLogotype from "../images/test-logo.svg";
 import video from "../images/video.mp4"
-// import backgroundLight from "../images/wallpapers/chelyabinsk-0.png";
-// import backgroundDark from "../images/wallpapers/chelyabinsk-1.png";
+import logotype from "../images/test-logo.svg";
 
 export default function Hero():React.ReactNode {
   return (
-    <section className="intro">
-      {/* <img src={(String(localStorage.getItem("theme")) === "dark" ? backgroundDark : backgroundLight)} alt=""/> */}
+    <section className="hero">
       <video className="video-bg" autoPlay muted loop playsInline>
         <source src={video} type="video/mp4"/>
       </video>
       <div className="info">
         <div className="info-container">
-          <span className="logotype" ><img src={testLogotype} alt="logotype.svg"/></span>
+          <span className="logotype" ><img src={logotype} alt="logotype.svg"/></span>
+          
           <nav>
             <h1>Экскурсия <br /> с доставкой</h1>
             <p>Присоединяйтесь к нашей экскурсии: погуляем по уютным улицам, узнаем город с новой стороны и просто хорошо проведем время вместе</p>
           </nav>
+        </div>
+
+        <div className="info-buttons">
+          <a href="app/">
+            Открыть приложение
+          </a>
         </div>
       </div>
     </section>
